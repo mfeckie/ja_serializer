77667ab5d1180a4050a4fbb2ad876b28b5ffc223
Martin Feckie <mfeckie@MuttonNet.local>
2017-04-26 21:29:36 +1000

    Remove unused alias

--- priv/templates/phx_gen_json_api/view.ex
+++ priv/templates/phx_gen_json_api/view.ex
@@ -2,14 +2,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect schema.alias %>View do
   use <%= inspect context.web_module %>, :view
   use JaSerializer.PhoenixView
 
-  alias <%= inspect context.web_module %>.<%= inspect schema.alias %>View
-
-
   attributes [<%= Map.keys(schema.types) |> Enum.map(&(":#{&1}")) |> Enum.join(", ") %>]
 
-  <%= for {ref, ref_id, _, _} <- schema.assocs do %>
-  has_one :<%= ref %>,
-    field: :<%= ref_id%>,
-    type: "<%= ref %>"<% end %>
-
+  <%= for {ref, ref_id, _, _} <- schema.assocs do %> has_one :<%= ref %>, field: :<%= ref_id%>, type: "<%= ref %>"<% end %>
 end
